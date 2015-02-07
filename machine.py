5b0e0564a1e8f18265205938d7c1b20e3eb991ea
slowinger <slowinger@uchicago.edu>
2015-02-07 12:33:37 -0600

    replaced new_total with total in machine.py

--- machine.py
+++ machine.py
@@ -2,10 +2,9 @@ class CoinDispenser:
     coins = [25, 10, 5, 1]
 
     def make_change(self, total):
-        new_total = total
         change = []
         for coin in self.coins:
-            coin_value = new_total // coin
-            new_total = new_total % coin
+            coin_value = total // coin
+            total = total % coin
             change.append(coin_value)
         return change
